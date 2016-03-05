8b788dd10ea02616d060921f42d812ee70bb1be3
Carlos Leite <carlosleitebos@gmail.com>
2016-03-05 15:21:13 -0300

    Estructura inicial

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,54 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require i18n
+//= require i18n/translations
+//= require ./libs/bootstrap.min.js
+
+
+// Reference: http://viget.com/inspire/extending-paul-irishs-comprehensive-dom-ready-execution
+CasaDeLasPinturas = {
+    common: {
+        init: function() {
+            // application-wide code
+
+
+        }
+    }
+
+};
+
+UTIL = {
+    exec: function( controller, action ) {
+        var ns = CasaDeLasPinturas,
+            action = ( action === undefined ) ? "init" : action;
+
+        if ( controller !== "" && ns[controller] && typeof ns[controller][action] == "function" ) {
+            ns[controller][action]();
+        }
+    },
+
+    init: function() {
+        var body = document.body,
+            controller = body.getAttribute( "data-controller" ),
+            action = body.getAttribute( "data-action" );
+
+        UTIL.exec( "common" );
+        UTIL.exec( controller );
+        UTIL.exec( controller, action );
+    }
+};
+
+$( document ).on('ready', UTIL.init );
