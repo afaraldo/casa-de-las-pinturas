8d27a0127264877732fb43cde04d1609773f38fc
Carlos Leite <carlosleitebos@gmail.com>
2016-03-07 14:27:21 -0300

    Fuentes actualizadas

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module CasaDeLasPinturas
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Agregar la carpeta fonts al pipeline
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
