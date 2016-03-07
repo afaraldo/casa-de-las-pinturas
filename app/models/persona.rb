8748ca9767b31cc7c15c9bb9f325f84317776767
Carlos Leite <carlosleitebos@gmail.com>
2016-03-07 13:47:41 -0300

    #9 Agregado validaciones para proveedores

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,3 +1,7 @@
 class Persona < ActiveRecord::Base
   validates :nombre, presence: true
+  validates :nombre, length: {maximum: 40}
+  validates :telefono, length: {maximum: 20}
+  validates :direccion, length: {maximum: 200}
+  validates :ruc, length: {maximum: 30}
 end
--- db/migrate/20160307023804_create_personas.rb
+++ db/migrate/20160307023804_create_personas.rb
@@ -1,11 +1,11 @@
 class CreatePersonas < ActiveRecord::Migration
   def change
     create_table :personas do |t|
-      t.string :nombre
-      t.string :telefono
-      t.string :direccion
-      t.string :ruc
-      t.string :type, null: false
+      t.string :nombre,     limit: 40
+      t.string :telefono,   limit: 20
+      t.string :direccion,  limit: 200
+      t.string :ruc,        limit: 30
+      t.string :type, null: false, limit: 15
       t.decimal :limite_credito
 
       t.timestamps null: false
