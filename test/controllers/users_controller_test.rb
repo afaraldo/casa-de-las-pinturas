6d5d1ef89bef86d815a56a2d0abdf36bc4f49b69
Adrian Agüero <iceberg.04@gmail.com>
2016-03-08 17:58:03 +0400

    Scaffold controller User

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -18,7 +18,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: {  }
+      post :create, user: { email: @user.email, name: @user.name, password: @user.password }
     end
 
     assert_redirected_to user_path(assigns(:user))
@@ -35,7 +35,7 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should update user" do
-    patch :update, id: @user, user: {  }
+    patch :update, id: @user, user: { email: @user.email, name: @user.name, password: @user.password }
     assert_redirected_to user_path(assigns(:user))
   end
 
