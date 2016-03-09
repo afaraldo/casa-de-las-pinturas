70e824d1e7c3ad86fbbfd1066531fc9dd488430a
mlored <loredelpuerto@gmail.com>
2016-03-09 12:12:52 -0300

    add sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :proveedores
 
   get 'welcome/index'
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
