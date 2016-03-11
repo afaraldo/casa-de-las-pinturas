ad2adcbfb38d98c79a7440660c3c98d0fed5cedb
Carlos Leite <carlosleitebos@gmail.com>
2016-03-10 21:21:12 -0300

    Actualizado estilos de las notificaciones de noty

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,35 +21,8 @@
 //= require ./libs/jquery.validation/additional-methods.js
 //= require ./libs/jquery.validation/messages_es_AR.js
 //= require ./libs/jquery.noty.packaged.js
+//= require ./libs/jquery.noty.defaults.js
 //= require_tree ./modules
-
-$.noty.defaults = {
-    layout: 'topRight',
-    theme: 'defaultTheme',
-    type: 'alert',
-    text: '', // can be html or string
-    dismissQueue: true, // If you want to use queue feature set this true
-    template: '<div class="noty_message"><span class="noty_text"></span><div class="noty_close"></div></div>',
-    animation: {
-        open: {height: 'toggle'},
-        close: {height: 'toggle'},
-        easing: 'swing',
-        speed: 500 // opening & closing animation speed
-    },
-    timeout: 5000, // delay for closing event. Set false for sticky notifications
-    force: false, // adds notification to the beginning of queue when set to true
-    modal: false,
-    maxVisible: 5, // you can set max visible notification for dismissQueue true option,
-    killer: false, // for close all notifications before show
-    closeWith: ['click'], // ['click', 'button', 'hover']
-    callback: {
-        onShow: function() {},
-        afterShow: function() {},
-        onClose: function() {},
-        afterClose: function() {}
-    },
-    buttons: false // an array of buttons
-};
 
 
 // Reference: http://viget.com/inspire/extending-paul-irishs-comprehensive-dom-ready-execution
