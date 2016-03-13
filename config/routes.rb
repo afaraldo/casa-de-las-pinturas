aa1f6cb6fefbbb24af7ebcafd8b70f68f9e40554
Carlos Leite <carlosleitebos@gmail.com>
2016-03-12 22:17:16 -0300

    arreglado routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,13 @@ Rails.application.routes.draw do
 
   resources :mercaderias
 
-  get 'welcome/list'
+  get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#list'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
