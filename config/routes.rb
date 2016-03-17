dba6be9063c717aa3705f8055136030dc1c798c7
lmoray <ludisdianam43@gmail.com>
2016-03-17 20:27:04 -0300

    #29 implemente la vista de inventario tiempo: 04h:30m

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get 'mercaderias/check_codigo' => 'mercaderias#check_codigo'
   resources :mercaderias
 
+  resources :inventarios
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
