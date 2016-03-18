bee1b45c85f0fa715e01ceb213c62a9ec8dd8b20
Carlos Leite <carlosleitebos@gmail.com>
2016-03-17 22:13:49 -0300

    Seed actualizado

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,4 +50,4 @@ end
 }
 
 # Crear un empleado
-@empleado = Persona.create nombre: 'Laura Perez', direccion:'Nuevo circuito comercial', telefono: '12345678', ruc: '1234567-8',  type: "Empleado", limite_credito: 0, user_attributes: { username: 'admin', email: 'admin@casadelaspinturas.com', password: '12345678' }
+@empleado = Persona.create nombre: 'Laura Perez', direccion:'Nuevo circuito comercial', telefono: '12345678', numero_documento: '1234567-8',  tipo: "Empleado", limite_credito: 0, user_attributes: { username: 'admin', email: 'admin@casadelaspinturas.com', password: '12345678' }
