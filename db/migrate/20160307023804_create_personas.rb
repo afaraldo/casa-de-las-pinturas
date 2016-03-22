066c25fabc5b4d17891a56f38e8b987dd0cb3d69
Adrian Agüero <iceberg.04@gmail.com>
2016-03-22 12:47:58 +0400

    Migracion que corrige el limite de los campos nombre, telefono y numero de documento de la tabla personas #6

--- db/migrate/20160307023804_create_personas.rb
+++ db/migrate/20160307023804_create_personas.rb
@@ -1,12 +1,13 @@
 class CreatePersonas < ActiveRecord::Migration
   def change
     create_table :personas do |t|
-      t.string :nombre,     limit: 150
-      t.string :telefono,   limit: 50
+      t.string :nombre,     limit: 40
+      t.string :telefono,   limit: 20
       t.string :direccion,  limit: 200
-      t.string :ruc,        limit: 20
+      t.string :ruc,        limit: 30
       t.string :type, null: false, limit: 15
       t.decimal :limite_credito
+
 
       t.timestamps null: false
     end
