427a88843c6d009a2ed6157c9799ce43146c0049
Adrian Agüero <iceberg.04@gmail.com>
2016-03-22 11:55:14 +0400

    Arreglo de tamaño del campo nombre en la tabla persona de 40 a 150 caracteres #6

--- db/migrate/20160307023804_create_personas.rb
+++ db/migrate/20160307023804_create_personas.rb
@@ -1,7 +1,7 @@
 class CreatePersonas < ActiveRecord::Migration
   def change
     create_table :personas do |t|
-      t.string :nombre,     limit: 40
+      t.string :nombre,     limit: 150
       t.string :telefono,   limit: 50
       t.string :direccion,  limit: 200
       t.string :ruc,        limit: 20
