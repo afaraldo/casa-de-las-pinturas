843659f6b289a788d99e84934e04bff2a5551e00
mlored <loredelpuerto@gmail.com>
2016-03-24 21:26:21 -0300

    #28 Implementar el ABM de Clientes Correción de validaciones de nombre de usuario #4 Revisión de consitencia con la base de datos agregué url al menu de ventas

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
          
   belongs_to :persona
   validates :username, presence: true
-  validates :username, length: {maximum: 45, minimum: 2}
+  validates :username, length: {maximum: 20, minimum: 2}
   validates :password, format: {with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+\z/, message: :password_format }
 
   validates_uniqueness_of :username, allow_blank: false
