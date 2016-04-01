5e121ac046d06307d08f0ecb38e8af1afc3cc27f
Carlos Leite <carlosleitebos@gmail.com>
2016-04-01 15:49:18 -0400

    #56 Agregado html para disenho de compras

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def compras_index
+
+  end
+
+  def compras_form
+    @movimiento = MovimientoMercaderia.new
+    @movimiento.detalles.build(mercaderia: Mercaderia.offset(rand(Mercaderia.count)).first)
+    @movimiento.detalles.build(mercaderia: Mercaderia.offset(rand(Mercaderia.count)).first)
+    @movimiento.detalles.build(mercaderia: Mercaderia.offset(rand(Mercaderia.count)).first)
+
+  end
 end
