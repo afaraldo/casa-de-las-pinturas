9a0874aa791583474a70a09638fdca4a35157f04
Carlos Leite <carlosleitebos@gmail.com>
2016-04-10 14:24:47 -0400

    Fixed migration

--- db/migrate/20160406212429_add_details_to_users.rb
+++ db/migrate/20160406212429_add_details_to_users.rb
@@ -1,5 +1,8 @@
 class AddDetailsToUsers < ActiveRecord::Migration
   def change
+    User.transaction do |u|
+      u.update(rol: (u.username == 'admin' ? 'superusuario' : 'administrador'))
+    end
     add_column :users, :rol, :string, limit: 20, null: false
     add_column :users, :deleted_at, :date
     add_index :users, :deleted_at
