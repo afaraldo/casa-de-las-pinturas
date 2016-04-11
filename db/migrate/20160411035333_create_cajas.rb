48224ebe1c282c348ac7141d407c714fb9e2c6b7
mlored <loredelpuerto@gmail.com>
2016-04-11 00:01:02 -0400

    #81 Implementar feature Movimientos de Caja migracion tabla cajas

---
+++ db/migrate/20160411035333_create_cajas.rb
@@ -0,0 +1,10 @@
+class CreateCajas < ActiveRecord::Migration
+  def change
+    create_table :cajas do |t|
+      t.string :nombre, limit: 50, null: false
+      t.datetime :deleted_at, null: true
+      
+      t.timestamps null: false
+    end
+  end
+end
