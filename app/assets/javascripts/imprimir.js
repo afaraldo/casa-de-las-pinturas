d6baf506ab5a95a2ed4fa5fa30fd5fdac0961ff9
Carlos Leite <carlosleitebos@gmail.com>
2016-04-25 23:09:13 -0400

    Dependencia actualizada

--- app/assets/javascripts/imprimir.js
+++ app/assets/javascripts/imprimir.js
@@ -2,7 +2,7 @@
 //= require jquery_ujs
 //= require i18n
 //= require i18n/translations
-//= require ./libs/Chart.js
 //= require ./libs/moment.js
+//= require ./libs/Chart.js
 //= require ./helpers/numberHelper.js
 //= require ./modules/reportes.js
