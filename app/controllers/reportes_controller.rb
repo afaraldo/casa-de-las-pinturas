1323d66ea60920c101f910060f6f044eb64f8887
mlored <loredelpuerto@gmail.com>
2016-05-09 01:13:50 -0400

    Merge branch 'development' into issue-97-implementarMovimientoMercaderiaStockInicial

--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -1,8 +1,8 @@
 class ReportesController < ApplicationController
 
-  layout 'imprimir', only: [:imprimir_reporte_compras, :imprimir_reporte_gastos, :imprimir_reporte_ventas]
+  layout 'imprimir', only: [:imprimir_reporte_compras, :imprimir_reporte_gastos, :imprimir_reporte_ventas, :imprimir_reporte_caja]
 
-  before_action :setup_menu, only: [:compras, :gastos, :ventas]
+  before_action :setup_menu, only: [:compras, :gastos, :ventas, :caja]
   before_action :setup_fechas
 
   # configuracion del menu
@@ -44,7 +44,21 @@ class ReportesController < ApplicationController
 
   def imprimir_reporte_gastos
     get_reporte_gastos
+    @categoria = CategoriaGasto.find_by(id: params[:categoria_gasto_id])
+
     render 'reportes/gastos/imprimir_reporte'
+  end
+
+  def caja
+    @menu_setup[:side_menu] = :reporte_caja
+
+    get_reporte_caja
+    render 'reportes/caja/reporte'
+  end
+
+  def imprimir_reporte_caja
+    get_reporte_caja
+    render 'reportes/caja/imprimir_reporte'
   end
 
   def setup_fechas
@@ -86,5 +100,19 @@ class ReportesController < ApplicationController
                               order_dir: params[:order_dir],
                               page: params[:page],
                               limit: action_name == 'imprimir_reporte_gastos' ? LIMITE_REGISTROS_IMPRIMIR : 100)
+  end
+
+  def get_reporte_caja
+    @reporte = CajaExtracto.reporte(desde: @desde,
+                                             hasta: @hasta,
+                                             caja_id: params[:caja_id],
+                                             moneda_id: params[:moneda_id],
+                                             agrupar_por: params[:agrupar_por],
+                                             resumido: params[:modo_resumido].present?,
+                                             order_by: params[:order_by],
+                                             order_dir: params[:order_dir],
+                                             page: params[:page],
+                                             limit: action_name == 'imprimir_reporte_caja' ? LIMITE_REGISTROS_IMPRIMIR : 100)
+
   end
 end
