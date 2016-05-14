3507564b67fae31602ef862038502e9ff6dde08a
mlored <loredelpuerto@gmail.com>
2016-05-14 03:23:58 -0400

    #100 agregué relaciones al modelo de cliente y metodos en cobros para integrar con los creditos de las devoluciones Tiempo utilizado: 5h (13/05/16)

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,6 +2,8 @@ class Cliente < Persona
   has_many :ventas, foreign_key: 'persona_id'
   has_many :ventas_pendientes, -> { where(estado: :pendiente) }, class_name: 'Venta', foreign_key: 'persona_id'
 
+  has_many :devoluciones_disponibles, -> { where.not(credito_restante: 0) }, class_name: 'NotasCreditosDebito', foreign_key: 'persona_id'
+
   validates :nombre, presence: true, length: {maximum: 150, minimum: 2}, uniqueness: true
   validates :numero_documento, format: { with: /\d+-?\d+\z/, message: :only_numbers_is_allowed }
 end
